48610e8618dcf8cd4c81bff7f346438c7655c925
AverageLeo <Arieka39@gmail.com>
2020-05-19 23:12:20 +0300

    created components

---
+++ src/components/LowNavbar/LowNavbar.js
@@ -0,0 +1,11 @@
+import React from 'react';
+
+const LowNavbar = () => {
+    return (
+        <div className="LowNavbar">
+            <h1>LowNavbar</h1>
+        </div>
+    );
+}
+
+export default LowNavbar;
---
+++ src/components/Picture/Picture.js
@@ -0,0 +1,11 @@
+import React from 'react';
+
+const Picture = () => {
+    return (
+        <div className="Picture">
+            <h1>Picture</h1>
+        </div>
+    );
+}
+
+export default Picture;
