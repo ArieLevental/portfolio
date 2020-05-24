9f2a433c163f1743c75af3aa2beca601f604c35c
AverageLeo <Arieka39@gmail.com>
2020-05-24 18:27:42 +0300

    Updates

---
+++ precache-manifest.1954ee6c5f11a246a3c7c80cd24e19d5.js
@@ -0,0 +1,46 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "8434f7a16c4bdba2f2f14afa65f70dff",
+    "url": "//averageleo.github.io/portfolio/index.html"
+  },
+  {
+    "revision": "87802af5e856e2285835",
+    "url": "//averageleo.github.io/portfolio/static/css/main.603d3add.chunk.css"
+  },
+  {
+    "revision": "24984dec255c635c5c6f",
+    "url": "//averageleo.github.io/portfolio/static/js/2.3f20e546.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "//averageleo.github.io/portfolio/static/js/2.3f20e546.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "87802af5e856e2285835",
+    "url": "//averageleo.github.io/portfolio/static/js/main.769c68a0.chunk.js"
+  },
+  {
+    "revision": "640fb401b20312d9d986",
+    "url": "//averageleo.github.io/portfolio/static/js/runtime-main.508ebc2d.js"
+  },
+  {
+    "revision": "be1fe3134d5bff7fb99d621201de9aed",
+    "url": "//averageleo.github.io/portfolio/static/media/HelveticaNowDisplay-Bold.be1fe313.ttf"
+  },
+  {
+    "revision": "53b5d05abde1c9192de6f6a954a5fef9",
+    "url": "//averageleo.github.io/portfolio/static/media/HelveticaNowDisplay-Medium.53b5d05a.ttf"
+  },
+  {
+    "revision": "9632144b89660f0f032a3a0a01e533c7",
+    "url": "//averageleo.github.io/portfolio/static/media/HelveticaNowDisplay-Regular.9632144b.ttf"
+  },
+  {
+    "revision": "f04809786c06c79badf9e787e7057d62",
+    "url": "//averageleo.github.io/portfolio/static/media/Logo.f0480978.png"
+  },
+  {
+    "revision": "c4b1e3920c880359a21252dda38f3979",
+    "url": "//averageleo.github.io/portfolio/static/media/picture.c4b1e392.png"
+  }
+]);
