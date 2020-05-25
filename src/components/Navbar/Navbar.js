febc3dc66a5860af0c97d9249b5992741e246877
AverageLeo <Arieka39@gmail.com>
2020-05-25 18:15:06 +0300

    responsive navbar

--- src/components/Navbar/Navbar.js
+++ src/components/Navbar/Navbar.js
@@ -3,29 +3,29 @@ import './Navbar.css';
 import myLogo from './Logo.png';
 
 const Navbar = () => {
-    return (
-        <div className="header">
-        <div className="logo">
+  return (
+    <div className="header">
+      <div className="logo">
         <img src={myLogo} alt=" " width="350" height="70" />
-        </div>
-        <div class="menu-wrap">
-            <input type="checkbox" class="toggler" />
-            <div class="hamburger"><p>Menu </p><div></div></div>
-            <div class="menu">
-                <div>
-                    <div>
-                        <ul>
-                            <li className="about__me"><a href="#about__me__p">About Me</a></li>
-                            <li className="my__projects"><a href="#my__projects_p">Projects</a></li>
-                            <li className="contact__me"><a href="#contact__me__p">Contact</a></li>
-                        </ul>
-                        <p className="copyrights">© 2020 Arie Levental</p>  
-                    </div>
-                </div>
+      </div>
+      <div class="menu-wrap">
+        <input type="checkbox" class="toggler" />
+        <div class="hamburger"><p>Menu</p><div></div></div>
+          <div class="menu">
+            <div>
+              <div>
+                <ul>
+                  <li className="about__me"><a href="#about__me__p">About Me</a></li>
+                  <li className="my__projects"><a href="#my__projects_p">Projects</a></li>
+                  <li className="contact__me"><a href="#contact__me__p">Contact</a></li>
+                </ul>
+                <p className="copyrights">© 2020 Arie Levental</p>  
+              </div>
             </div>
-        </div>
-        </div>
-    );
+          </div>
+      </div>
+    </div>
+  );
 }
 
 export default Navbar;
