f7b8f8a98e2f771728d90955c9e18c342dfff8b0
AverageLeo <Arieka39@gmail.com>
2020-05-25 18:58:11 +0300

    adjusted about me section

--- src/components/Picture/Picture.js
+++ src/components/Picture/Picture.js
@@ -3,24 +3,24 @@ import './Picture.css';
 import myPicture from './picture.png';
 
 const Picture = () => {
-    return (
-        <div className='intro'>
-        <p id="about__me__p"></p>
-            <div className="picture">
-                <img src={myPicture} alt=" " width="340" height="500" />
-            </div>
-            <div className="text">
-                <p id="name">Arie Levental</p>
-                <p id="title">Full-Stack <br /> Web Developer</p>
-                <p id="about">Full Stack developer, capable of developing web platforms from scratch to production, with full responsiveness and preformance. 
-                <br />
-                I focus on planning, designing and developing both Back-End and Front-End.
-                <br />
-                Hands-on a wide range of technologies and platforms:
-                JaveScript, CSS, HTML, React, Redux, PostgreSQL, Node.js and Adobe Photoshop.</p>
-            </div>
-        </div>
-    );
+  return (
+    <div className='intro'>
+      <p id="about__me__p"></p>
+      <div className="picture">
+        <img src={myPicture} alt=" " width="340" height="500" />
+      </div>
+      <div className="text">
+          <p id="name">Arie Levental</p>
+          <p id="title">Full-Stack <br /> Web Developer</p>
+          <p id="about">Full Stack developer, capable of developing web platforms from scratch to production, with full responsiveness and preformance. 
+            <br />
+            I focus on planning, designing and developing both Back-End and Front-End.
+            <br />
+            Hands-on a wide range of technologies and platforms:
+            JaveScript, CSS, HTML, React, Redux, PostgreSQL, Node.js and Adobe Photoshop.</p>
+      </div>
+    </div>
+  );
 }
 
 export default Picture;
