79e8e6264fd0c34e0b7b9a28fad7df6e53ce82bb
AverageLeo <Arieka39@gmail.com>
2020-06-14 17:35:27 +0300

    contact form

--- src/components/Contact/Contact.js
+++ src/components/Contact/Contact.js
@@ -1,19 +1,68 @@
-import React from 'react';
-import './Contact.css';
+import React from "react";
+import "./Contact.css";
+import firebase from "firebase";
 
 const Contact = () => {
-    return (
-        <div className="contact">
-            <p id="contact__me__p"></p>
-            <h1>Contact</h1>
-            <h3>I'm eager to learn new things and improve myself. 
-            I'm currently looking for my next challenge so feel free to reach out to me any time.</h3>
-            <div className='icons'>
-                <div className="icons__circle"> <a href="mailto:arieka39@gmail.com"><i className="far fa-envelope icons__color"></i></a></div>
-                <div className="icons__circle"> <a href="https://www.linkedin.com/in/arie-levental/"><i className="fab fa-linkedin-in icons__color"></i></a></div>
-            </div>
+  return (
+    <div className="contact">
+      <p id="contact__me__p"></p>
+      <div className="contact-text">
+        <h1>Contact</h1>
+        <h3>
+          I'm eager to learn new things and improve myself. I'm currently
+          looking for my next challenge so feel free to reach out to me any
+          time.
+        </h3>
+      </div>
+      <div class="container">
+        <div class="wrapper animated bounceInLeft">
+          <div class="contact-info">
+            <h3>Arie Levental</h3>
+            <ul>
+              <li>
+                <i class="fa fa-road"></i> Israel, Jerusalem / Tel-Aviv
+              </li>
+              <li>
+                <i class="fa fa-phone"></i> (972) 55-666-5679
+              </li>
+              <li>
+                <i class="fa fa-envelope"></i> Arieka39@gmail.com
+              </li>
+            </ul>
+          </div>
+          <div class="alert">Your message has been sent</div>
+          <div class="contact-text-area">
+            <h3>Email Me</h3>
+            <form id="contactForm">
+              <p>
+                <label>Name</label>
+                <input type="text" name="name" id="name" />
+              </p>
+              <p>
+                <label>Company</label>
+                <input type="text" name="company" id="company" />
+              </p>
+              <p>
+                <label>Email Address</label>
+                <input type="email" name="email" id="email" />
+              </p>
+              <p>
+                <label>Phone Number</label>
+                <input type="text" name="phone" id="phone" />
+              </p>
+              <p class="full">
+                <label>Message</label>
+                <textarea name="message" rows="5" id="message"></textarea>
+              </p>
+              <p class="full">
+                <button>Submit</button>
+              </p>
+            </form>
+          </div>
         </div>
-    );
-}
+      </div>
+    </div>
+  );
+};
 
 export default Contact;
--- src/components/Home/Home.js
+++ src/components/Home/Home.js
@@ -1,46 +1,57 @@
-import React from 'react';
-import './Home.scss';
-import myPicture from './picture.png';
+import React from "react";
+import "./Home.scss";
+import myPicture from "./picture.png";
 
 const Home = () => {
   return (
-    <div className='intro'>
-      <div className="marquee lax" data-lax-opacity="0 1, (document.body.scrollHeight*0.2) 0">
-       <span>DEVELOPER</span>
+    <div className="intro">
+      <div
+        className="marquee lax"
+        data-lax-opacity="0 1, (document.body.scrollHeight*0.2) 0"
+      >
+        <span>DEVELOPER</span>
       </div>
       <div className="cursor"></div>
       <p id="about__me__p"></p>
-      <div className="picture lax" data-lax-opacity="0 1, (document.body.scrollHeight*0.2) 0">
+      <div
+        className="picture lax"
+        data-lax-opacity="0 1, (document.body.scrollHeight*0.2) 0"
+      >
         <img src={myPicture} alt="" />
       </div>
       <div className="home-text">
         <p className="home-text-name">Arie Levental</p>
-        <p className="home-text-title">Full-Stack <br /> Web Developer</p>
-        <p className="home-text-about">Full Stack <span className="fancy__color">developer</span>, capable of developing <span className="fancy__color">web platforms</span> from scratch to production, with full responsiveness and preformance. 
-        <br />
-        I focus on planning, <span className="fancy__color">designing</span> and developing both Back-End and Front-End.
-        <br />
-        Hands-on a wide range of technologies and platforms:
-        JaveScript, CSS, HTML, React, Redux, PostgreSQL, Node.js and Adobe Photoshop.</p>
+        <p className="home-text-title">
+          Full-Stack <br /> Web Developer
+        </p>
+        <p className="home-text-about">
+          Full Stack <span className="fancy__color">developer</span>, capable of
+          developing <span className="fancy__color">web platforms</span> from
+          scratch to production, with full responsiveness and preformance.
+          <br />I focus on planning,{" "}
+          <span className="fancy__color">designing</span> and developing both
+          Back-End and Front-End.
+          <br />
+          Hands-on a wide range of technologies and platforms: JaveScript, CSS,
+          HTML, React, Redux, PostgreSQL, Node.js and Adobe Photoshop.
+        </p>
       </div>
-          <div className="scroll__icon">
-          <div className="wheel__icon">
-          <div className="mouse__icon">
-          </div>
-          </div>
-          </div>
-    </div> 
-    );  
-  }  
-  
-  
-  window.addEventListener('mousemove', cursor);
-  
-  function cursor(e) {
-    const mouseCursor = document.querySelector('.cursor');
+      <div className="scroll__icon">
+        <div className="wheel__icon">
+          <div className="mouse__icon"></div>
+        </div>
+      </div>
+    </div>
+  );
+};
 
-    mouseCursor.style.top = e.clientY  -10 + 'px';
-    mouseCursor.style.left = e.clientX  -10 + 'px';
-  }
-  
+window.addEventListener("mousemove", cursor);
+
+function cursor(e) {
+  const mouseCursor = document.querySelector(".cursor");
+
+  mouseCursor.style.top = e.clientY - 10 + "px";
+  mouseCursor.style.left = e.clientX - 10 + "px";
+}
+
 export default Home;
