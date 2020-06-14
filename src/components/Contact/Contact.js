ce1fd038d6fccd41a5ac99163e2abc587b6fe680
AverageLeo <Arieka39@gmail.com>
2020-06-15 00:22:44 +0300

    contact form is now directly connected to my mail

--- src/components/Contact/Contact.js
+++ src/components/Contact/Contact.js
@@ -1,68 +1,103 @@
 import React from "react";
 import "./Contact.css";
-import firebase from "firebase";
 
-const Contact = () => {
-  return (
-    <div className="contact">
-      <p id="contact__me__p"></p>
-      <div className="contact-text">
-        <h1>Contact</h1>
-        <h3>
-          I'm eager to learn new things and improve myself. I'm currently
-          looking for my next challenge so feel free to reach out to me any
-          time.
-        </h3>
-      </div>
-      <div class="container">
-        <div class="wrapper animated bounceInLeft">
-          <div class="contact-info">
-            <h3>Arie Levental</h3>
-            <ul>
-              <li>
-                <i class="fa fa-road"></i> Israel, Jerusalem / Tel-Aviv
-              </li>
-              <li>
-                <i class="fa fa-phone"></i> (972) 55-666-5679
-              </li>
-              <li>
-                <i class="fa fa-envelope"></i> Arieka39@gmail.com
-              </li>
-            </ul>
-          </div>
-          <div class="alert">Your message has been sent</div>
-          <div class="contact-text-area">
-            <h3>Email Me</h3>
-            <form id="contactForm">
-              <p>
-                <label>Name</label>
-                <input type="text" name="name" id="name" />
-              </p>
-              <p>
-                <label>Company</label>
-                <input type="text" name="company" id="company" />
-              </p>
-              <p>
-                <label>Email Address</label>
-                <input type="email" name="email" id="email" />
-              </p>
-              <p>
-                <label>Phone Number</label>
-                <input type="text" name="phone" id="phone" />
-              </p>
-              <p class="full">
-                <label>Message</label>
-                <textarea name="message" rows="5" id="message"></textarea>
-              </p>
-              <p class="full">
-                <button>Submit</button>
-              </p>
-            </form>
+class Contact extends React.Component {
+  constructor(props) {
+    super(props);
+    this.submitForm = this.submitForm.bind(this);
+    this.state = {
+      status: "",
+    };
+  }
+
+  render() {
+    const { status } = this.state;
+    return (
+      <div className="contact">
+        <p id="contact__me__p"></p>
+        <div className="contact-text">
+          <h1>Contact</h1>
+          <h3>
+            I'm eager to learn new things and improve myself. I'm currently
+            looking for my next challenge so feel free to reach out to me any
+            time.
+          </h3>
+        </div>
+        <div className="container">
+          <div className="wrapper animated bounceInLeft">
+            <div className="contact-info">
+              <h3>Arie Levental</h3>
+              <ul>
+                <li>
+                  <i className="fa fa-road"></i> Israel, Jerusalem / Tel-Aviv
+                </li>
+                <li>
+                  <i className="fa fa-phone"></i> (972) 55-666-5679
+                </li>
+                <li>
+                  <i className="fa fa-envelope"></i> Arieka39@gmail.com
+                </li>
+              </ul>
+            </div>
+            <div className="contact-text-area">
+              <h3>Email Me</h3>
+              <form
+                onSubmit={this.submitForm}
+                action="https://formspree.io/maypjazw"
+                method="POST"
+              >
+                <p>
+                  <label>Name</label>
+                  <input type="text" name="name" required={true} />
+                </p>
+                <p>
+                  <label>Company</label>
+                  <input type="text" name="company" />
+                </p>
+                <p>
+                  <label>Email Address</label>
+                  <input type="email" name="email" required={true} />
+                </p>
+                <p>
+                  <label>Phone Number</label>
+                  <input type="text" name="phone" />
+                </p>
+                <p className="full">
+                  <label>Message</label>
+                  <textarea name="message" rows="5" required={true}></textarea>
+                </p>
+                {status === "SUCCESS" ? (
+                  <p className="submit-success full">I got your messege!</p>
+                ) : (
+                  <button className="full">Submit</button>
+                )}
+                {status === "ERROR" && <p>Ooops! There was an error.</p>}
+              </form>
+            </div>
           </div>
         </div>
       </div>
-    </div>
-  );
-};
+    );
+  }
+
+  submitForm(ev) {
+    ev.preventDefault();
+    const form = ev.target;
+    const data = new FormData(form);
+    const xhr = new XMLHttpRequest();
+    xhr.open(form.method, form.action);
+    xhr.setRequestHeader("Accept", "application/json");
+    xhr.onreadystatechange = () => {
+      if (xhr.readyState !== XMLHttpRequest.DONE) return;
+      if (xhr.status === 200) {
+        form.reset();
+        this.setState({ status: "SUCCESS" });
+      } else {
+        this.setState({ status: "ERROR" });
+      }
+    };
+    xhr.send(data);
+  }
+}
 
 export default Contact;
--- src/components/Navbar/Navbar.js
+++ src/components/Navbar/Navbar.js
@@ -1,6 +1,6 @@
-import React from 'react';
-import './Navbar.css';
-import myLogo from './Logo.png';
+import React from "react";
+import "./Navbar.css";
+import myLogo from "./Logo.png";
 
 const Navbar = () => {
   return (
@@ -10,27 +10,39 @@ const Navbar = () => {
       </div>
       <div className="menu-wrap">
         <input type="checkbox" className="toggler" />
-        <div className="hamburger"><p>Menu</p><div></div></div>
-          <div className="menu">
+        <div className="hamburger">
+          <p>Menu</p>
+          <div></div>
+        </div>
+        <div className="menu">
+          <div>
             <div>
-              <div>
-                <ul>
-                  <li className="navItem about__me" onClick={navbarClicked}><a href="#about__me__p">About Me</a></li>
-                  <li className="navItem my__projects" onClick={navbarClicked}><a href="#my__projects_p">Projects</a></li>
-                  <li className="navItem contact__me" onClick={navbarClicked}><a href="#contact__me__p">Contact</a></li>
-                  <li className="resume"><a href=" ">Resume </a><i className="fas fa-file-download fa-xs"></i></li>
-                </ul>
-                <p className="copyrights">© 2020 Arie Levental</p>  
-              </div>
+              <ul>
+                <li className="navItem about__me" onClick={navbarClicked}>
+                  <a href="#about__me__p">About Me</a>
+                </li>
+                <li className="navItem my__projects" onClick={navbarClicked}>
+                  <a href="#my__projects_p">Projects</a>
+                </li>
+                <li className="navItem contact__me" onClick={navbarClicked}>
+                  <a href="#contact__me__p">Contact</a>
+                </li>
+                <li className="resume">
+                  <a href=" ">Resume </a>
+                  <i className="fas fa-file-download fa-xs"></i>
+                </li>
+              </ul>
+              <p className="copyrights">© 2020 Arie Levental</p>
             </div>
           </div>
+        </div>
       </div>
     </div>
   );
-}
+};
 
 function navbarClicked() {
-  const toggler = document.getElementsByClassName('toggler')[0];
+  const toggler = document.getElementsByClassName("toggler")[0];
   toggler.checked = false;
 }
 
