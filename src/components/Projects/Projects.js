23dee6c2db43389ed671a92a088c0cc98c566d73
AverageLeo <Arieka39@gmail.com>
2020-08-10 12:22:29 +0300

    added 2 project cards

--- src/components/Projects/Projects.js
+++ src/components/Projects/Projects.js
@@ -34,10 +34,120 @@ const Projects = () => {
                   <span>PostgreSQL</span>
                   <span>Bcrypt</span>
                   <span>ClarifaiAPI</span>
+                  <span>Heroku</span>
                 </div>
                 <div className="project-buttons">
-                  <a href="https://eagle-vision.herokuapp.com/">Live Demo</a>
-                  <a href="https://github.com/AverageLeo/eagle-vision">
+                  <a
+                    href="https://eagle-vision.herokuapp.com/"
+                    target="_blank"
+                    rel="noopener noreferrer"
+                  >
+                    Live Demo
+                  </a>
+                  <a
+                    href="https://github.com/AverageLeo/eagle-vision"
+                    target="_blank"
+                    rel="noopener noreferrer"
+                  >
+                    GitHub
+                  </a>
+                </div>
+              </div>
+            </div>
+          </div>
+          <div className="card fruitty">
+            <div className="enter-icon">
+              <img src={next} alt="" />
+            </div>
+            <div className="card-bg">
+              <div className="card-text">
+                <h1>Fruitty</h1>
+                <h3>
+                  RoboFriends displays cards of robots with image, name and
+                  email. It fetches data from an API.
+                </h3>
+                <div className="project-techs">
+                  <span>React.js</span>
+                  <span>React-router</span>
+                  <span>Redux</span>
+                  <span>Node.js</span>
+                  <span>Express.js</span>
+                  <span>Styled-Components</span>
+                  <span>JWToken</span>
+                  <span>MongoDB / Mongoose</span>
+                </div>
+                <div className="project-buttons">
+                  <a>Live Demo - Soon</a>
+                  <a
+                    href="https://github.com/AverageLeo/fruitty"
+                    target="_blank"
+                    rel="noopener noreferrer"
+                  >
+                    GitHub
+                  </a>
+                </div>
+              </div>
+            </div>
+          </div>
+          <div className="card portfolio">
+            <div className="enter-icon">
+              <img src={next} alt="" />
+            </div>
+            <div className="card-bg">
+              <div className="card-text">
+                <h1>Personal Porfolio</h1>
+                <h3>
+                  My Portfolio, built from scratch to showcase my design
+                  abilities and my experience. Full compatibility for desktop,
+                  tablet and mobile devices. Much fun.
+                </h3>
+                <div className="project-techs">
+                  <span>React.js</span>
+                  <span>Sass</span>
+                  <span>Firebase</span>
+                </div>
+                <div className="project-buttons">
+                  <a
+                    href="https://arielevental.com/"
+                    target="_blank"
+                    rel="noopener noreferrer"
+                  >
+                    Live Demo
+                  </a>
+                  <a
+                    href="https://github.com/AverageLeo/portfolio"
+                    target="_blank"
+                    rel="noopener noreferrer"
+                  >
+                    GitHub
+                  </a>
+                </div>
+              </div>
+            </div>
+          </div>
+          <div className="card mcdownloads">
+            <div className="enter-icon">
+              <img src={next} alt="" />
+            </div>
+            <div className="card-bg">
+              <div className="card-text">
+                <h1>McDownload's</h1>
+                <h3>
+                  A restaurant tool built with React, build your custom burger,
+                  get an order summary and confirm your order!
+                </h3>
+                <div className="project-techs">
+                  <span>React.js</span>
+                  <span>React Router</span>
+                  <span>CSS Modules</span>
+                </div>
+                <div className="project-buttons">
+                  <a href="/">Live Demo - Soon</a>
+                  <a
+                    href="https://github.com/AverageLeo/McDownloads"
+                    target="_blank"
+                    rel="noopener noreferrer"
+                  >
                     GitHub
                   </a>
                 </div>
@@ -61,34 +171,20 @@ const Projects = () => {
                   <span>Tachyons</span>
                 </div>
                 <div className="project-buttons">
-                  <a href="https://averageleo.github.io/robofriends/">
+                  <a
+                    href="https://averageleo.github.io/robofriends/"
+                    target="_blank"
+                    rel="noopener noreferrer"
+                  >
                     Live Demo
                   </a>
-                  <a href="https://github.com/AverageLeo/robofriends">GitHub</a>
-                </div>
-              </div>
-            </div>
-          </div>
-          <div className="card portfolio">
-            <div className="enter-icon">
-              <img src={next} alt="" />
-            </div>
-            <div className="card-bg">
-              <div className="card-text">
-                <h1>Personal Porfolio</h1>
-                <h3>
-                  My Portfolio, built from scratch to showcase my design
-                  abilities and my experience. Full compatibility for desktop,
-                  tablet and mobile devices. Much fun.
-                </h3>
-                <div className="project-techs">
-                  <span>React.js</span>
-                  <span>Sass</span>
-                  <span>Firebase</span>
-                </div>
-                <div className="project-buttons">
-                  <a href="https://arielevental.com/">Live Demo</a>
-                  <a href="https://github.com/AverageLeo/portfolio">GitHub</a>
+                  <a
+                    href="https://github.com/AverageLeo/robofriends"
+                    target="_blank"
+                    rel="noopener noreferrer"
+                  >
+                    GitHub
+                  </a>
                 </div>
               </div>
             </div>
